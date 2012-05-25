8148fe88a5482ed0bc5e2138aae4dbfa2b895438
Dextersmart Harry <dextersmart@gmail.com>
2012-05-25 18:04:28 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thefutureacad::Application.config.secret_token = '89e3e96ab354368315ab25dfd724e8ffbfa836b49a4f4c40ce710dfba26874c3a3f141b71b5454b54bf857958f2867e4551b2c19949b08c05c951af7c054d456'
